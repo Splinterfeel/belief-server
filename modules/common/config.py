917602f0121361667c608646e3c9937cb935ef96
Splinterfeel <skkiminock@mail.ru>
2024-05-04 13:31:24 +0300

    add CELLS_IN_STRONGHOLD to config

--- modules/common/config.py
+++ modules/common/config.py
@@ -3,13 +3,15 @@ from sqlalchemy import select
 from orm.common import Config
 
 MAX_STRONGHOLDS_PER_CHUNK = 2
+CELLS_IN_STRONGHOLD = 25
 
 
 def init():
-    global MAX_STRONGHOLDS_PER_CHUNK
+    global MAX_STRONGHOLDS_PER_CHUNK, CELLS_IN_STRONGHOLD
     with Session() as session:
         params = session.execute(select(Config)).scalars().all()
         config = dict()
     for p in params:
         config[p.param] = p.value
     MAX_STRONGHOLDS_PER_CHUNK = int(config['MAX_STRONGHOLDS_PER_CHUNK'])
+    CELLS_IN_STRONGHOLD = int(config['CELLS_IN_STRONGHOLD'])
